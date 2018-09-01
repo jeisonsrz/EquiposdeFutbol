3318b07d0dfd438a26a2ac0dd4af2391d9bc03c1
jeisonsrz <jeison.ruiz.z@uniautonoma.edu.co>
2018-09-01 08:28:42 -0500

    Segundo Commit Sabado

--- equiposFutbol.js
+++ equiposFutbol.js
@@ -1,22 +1,66 @@
 var dataEquipos=[
 
   {
+    estrellitas: "🌟🌟🌟🌟🌟",
     nombre: "Nacional",
-    urlimage: "",
-    dt: "Almidon",
+    urlimage: "logoNacional.png",
+    dt: "Almidon Nacional",
     juegadores: ["matias","carebata","panseroti","carroLoco","yucaFrita"],
     reseña: "El ",
-    estadio: "",
-    estrellas: "4",
+    estadio: "Estadio Atanasio Girardot",
+    estrellas: "eNacional.png",
   },
   {
+    estrellitas: "🌟🌟🌟",
     nombre: "America",
-    urlimage: "",
-    dt: "Americanito",
+    urlimage: "logoAmerica.png",
+    dt: "Americanito DT",
     juegadores: ["Caballon","Arepudo","Salchipapa","Mazamorra","Rosconsito"],
     reseña: "El americano",
-    estadio: "",
-    estrellas: "2",
+    estadio: "Estadio Pascual Guerrero",
+    estrellas: "eAmerica.png",
+  },
+  {
+    estrellitas: "🌟",
+    nombre: "Millonarios",
+    urlimage: "logoMillonarios.png",
+    dt: "Millonariesito DT",
+    juegadores: ["Millonariesito1","Millonariesito2","Millonariesito3","Millonariesito4","Millonariesito5"],
+    reseña: "El americano",
+    estadio: "Estadio El Campín",
+    estrellas: "eMillonarios.png",
+  },
+  {
+    estrellitas: "🌟🌟🌟🌟🌟🌟🌟🌟",
+    nombre: "Tolima",
+    urlimage: "logoTolima.png",
+    dt: "Tolimita DT",
+    juegadores: ["Tolimon","Tolimudo","Tolinsote","Totoludo","Tokimon"],
+    reseña: "El americano",
+    estadio: "Estadio Manuel Murillo Toro",
+    estrellas: "eTolima.png",
+  }
+  ,
+  {
+    estrellitas: "🌟🌟🌟🌟",
+    nombre: "Medellin",
+    urlimage: "logoMedellin.png",
+    dt: "Medellinsito DT",
+    juegadores: ["Medellino","Medilludo","Medillunsote","Medillinito","Medillonote"],
+    reseña: "El americano",
+    estadio: "El medillinsito Estadio Atanasio Girardot",
+    estrellas: "eMedellin.png",
+  }
+  ,
+  {
+    estrellitas: "🌟🌟",
+    nombre: "Pasto",
+    urlimage: "logoPasto.png",
+    dt: "Pastusito DT",
+    juegadores: ["Paston","Pastusin","Pastico","Pastisal","Pastote"],
+    reseña: "El americano",
+    estadio: "Estadio Departamental Libertad",
+    estrellas: "ePasto.png",
   }
 
 
@@ -27,13 +71,22 @@ var dataEquipos=[
 var selEquipos = document.getElementById("equipos");
 var nomEquipo = document.getElementById("nombre");
 var imagenEquipo = document.getElementById("imgEquipo");
-var imagenesEquipos = ["logoNacional.png","logoAmerica.png","logoMillonarios.png"]
+var imagenesEquipos = []
+var dt=document.getElementById("dt");
+var jugadores=document.getElementById("jugadores");
+var estadio=document.getElementById("estadio");
+var estrellas=document.getElementById("estrellasEquipo");
+var estrellitas=document.getElementById("estrellitas");
 
 selEquipos.onchange = function ()
 {
  //document.getElementById("nombre").innerHTML = this.value;
 //console.log();
 nomEquipo.innerHTML= this.options[this.selectedIndex].text;
-imagenEquipo.setAttribute("src",imagenesEquipos[this.selectedIndex]);
+imagenEquipo.setAttribute("src",dataEquipos[this.selectedIndex].urlimage);
+dt.innerHTML=dataEquipos[this.selectedIndex].dt;
+jugadores.innerHTML=dataEquipos[this.selectedIndex].juegadores;
+estadio.innerHTML=dataEquipos[this.selectedIndex].estadio;
+estrellitas.innerHTML=dataEquipos[this.selectedIndex].estrellitas;
 
 }
